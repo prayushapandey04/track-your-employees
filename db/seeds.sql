4e2c170a9d60ce8b9eb749ca7ffc9bab4f90b2fa
prayushapandey04 <prayusha.pandey4@gmail.com>
2023-12-04 17:51:59 -0600

    working on seeds file

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+USE SQLchallenge
+
+INSERT INTO employees (name, department, position) VALUES
+
+('John Doe', 'IT', 'Software Developer'),
+('Jane Smith', 'HR', 'HR Specialist'),
+('Bob Ross', 'Finance', 'Financial Analyst'),
+('Mike Ross', 'Marketing', 'Marketing Coordinator'),
+('Ana Williams', 'Engineering', 'Petroleium Engineer');
+
+
---
+++ server.js
@@ -0,0 +1,13 @@
+const mysql = require('mysql');
+
+// Creating my database connection
+
+const connection = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    password: 'Password123',
+    database: ''
+});
+
+
+
